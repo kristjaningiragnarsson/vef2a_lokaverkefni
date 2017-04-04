e15c1d4dc512702fa562a4c58d459df00b4d23b1
kristjaningiragnarsson <kristjanra210@nemi.tskoli.is>
2017-04-04 09:25:17 +0000

    database koði

---
+++ database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `users` (
+  `id` int(10) not null auto_increment PRIMARY KEY,
+  `nafn` varchar(20)NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL
+);
+create table images 
+(
+  id int not null auto_increment PRIMARY KEY,
+  image_name varchar(45) not null,
+  image_path varchar(25) not null,
+  image_text varchar(120) default null
+);

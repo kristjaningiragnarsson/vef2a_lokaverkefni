94a391e2aa826b367602691a30cf92d5925cc019
kristjaningiragnarsson <kristjanra210@nemi.tskoli.is>
2017-04-04 09:09:07 +0000

    index siða

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Lokaverkefni Vef2a - Myndavefur</title>
+</head>
+
+<body>
+<h1>Lokaverkefni Vef2a - Myndavefur</h1>
+				velkominn<br>
+				<a href="login.php">skrá inn</a> <br>
+				<a href="register.php">skráning</a> <br>
+		
+</body>
+</html>
